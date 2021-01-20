4ce2f6153cce53628aa8f35763310aff5f516c32
jarabeenoi <nongnat61@gmail.com>
2021-01-20 17:42:22 +0700

    update_booking

--- src/page/Front_endTest.js
+++ src/page/Front_endTest.js
@@ -1,118 +1,292 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import { Box } from '@material-ui/core';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 
 const useStyle = makeStyles({
-   // mainContainer: {
-   //    width: "1671px",
-   //    height: "1253px",
-   //    background: "radial - gradient(75.44 % 75.44 % at 30.03 % 0 %, rgba(255, 255, 255, 0.2) 0 %, rgba(255, 255, 255, 0) 100 %), linear- gradient(236.58deg, #BCBFC8 10.31 %, #91A2C6 90.59 %), #B9BDC8",
-   // },
    container: {
       width: "1479px",
       height: "1004px",
       display: "flex",
-      //margin: "123px 0 126px 96px",
       margin: "0 auto",
-      background: "#EFEEEC",
    },
    left: {
       width: "585px",
       height: "1004px",
       background: "#46529D",
+      display: "flex",
+   },
+   lh: {
+      marginLeft: "90px",
    },
    leftHeader: {
       width: "495px",
       height: "135px",
-      marginLeft: "90px",
       background: "#2EBAEE",
       boxShadow: "0px 4px 32px rgba(57, 64, 111, 0.25)",
+      overflow: "hidden",
    },
-   room: {
-      width: "130px",
-      height: "54px",
-      paddingTop: "56px",
-      paddingLeft:"48px",
-      fontFamily: "Roboto",
-      fontSstyle: "normal",
+   roomLabel: {
+      marginTop: "56px",
+      marginLeft: "48px",
       fontWeight: "bold",
       fontSize: "54px",
       lineHeight: "100%",
       color: "#FFFFFF",
    },
+   upComing: {
+      marginTop: "125px",
+      fontSize: "18px",
+      lineHeight: " 100%",
+      color: "#FFFFFF",
+   },
+   day: {
+      width: "226px",
+      height: "64px",
+      marginTop: "58px",
+      fontWeight: "300",
+      fontSize: "64px",
+      lineHeight: "100%",
+      color: "#FFFFFF",
+      opacity: "0.5",
+   },
+   date: {
+      marginTop: "15px",
+      fontWeight: "300",
+      fontSize: "64px",
+      lineHeight: "100%",
+      color: "#FFFFFF",
+   },
+   bookingDetail: {
+      marginTop: "90px",
+   },
+   bookingList: {
+      marginBottom: "38px",
+   },
+   time: {
+      fontSize: "16px",
+      lineHeight: "100%",
+      color: "#FFFFFF",
+      opacity: "0.5",
+   },
+   bookingTitle: {
+      marginTop: "8px",
+      fontSize: "20px",
+      lineHeight: "100%",
+      color: "#FFFFFF",
+   },
    right: {
       width: "100%",
-      background: "#FFFFFF",
       boxShadow: "0px 0px 30px rgba(0, 0, 0, 0.14)",
    },
    rightHeader: {
       height: "135px",
       background: "#EFEEEC",
+      display: "flex",
+      justifyContent: "flex-end",
+      flexDirection: "column",
+      paddingBottom: "28px"
    },
-   week: {
-      paddingTop: "83px",
-      paddingLeft: "200px"
+   menu: {
+      marginLeft: "64px",
+      lineHeight: "100%",
+      fontSize: "24px",
+      textTransform: "uppercase",
    },
-   today: {
+   menuLink: {
+      textDecoration: "none",
+   },
+   menuLinkGap: {
+      paddingLeft: "75px",
+   },
+   dayLabel: {
+      marginTop: "78px",
+      display: "flex",
+      alignItems: "center",
       background: "#F7F7F7",
       border: "1px solid #ECECEC",
-      boxSizing: "border-box",
-      position: "absolute",
-      width: "893px",
+      width: "100%",
       height: "47px",
-      left: "682px",
-      top: "336px",
-   }
-
+   },
+   dateLabel: {
+      marginLeft: "94px",
+      fontSize: "18px",
+      fontWeight: "bold",
+      color: "#787878",
+      lineHeight: "100%",
+   },
+   bookingDetailRight: {
+      marginTop: "50px",
+   },
+   bookingListRight: {
+      marginBottom: "38px",
+      display: "flex",
+   },
+   marking: {
+      marginLeft: "54px",
+   },
+   timeRight: {
+      fontSize: "16px",
+      lineHeight: "100%",
+      color: "#000",
+      opacity: "0.5",
+   },
+   bookingTitleRight: {
+      marginTop: "8px",
+      fontSize: "20px",
+      lineHeight: "100%",
+      color: "#000",
+   },
 })
-
 
 function Front_endTest() {
    const classes = useStyle();
 
    return (
-      <div >
+      <div>
          <div className={classes.container}>
             <div className={classes.left}>
-               <div className={classes.leftHeader}>
-                  <Typography variant="h3" className={classes.room}>
-                     A101
-                  </Typography>
-               </div>
-               <div>
-                  <p>Upcoming</p>
-                  <h1>Monday</h1>
-                  <h2>28 sep</h2>
-               </div>
-               <div>
-                  <div>
-                     <p>13.00 - 14.00</p>
-                     <p>Lunch with Petr</p>
+               <div className={classes.lh}>
+                  <div className={classes.leftHeader}>
+                     <Typography className={classes.roomLabel}>
+                        A101
+                     </Typography>
                   </div>
 
+                  <Typography className={classes.upComing}>
+                     Upcoming
+                  </Typography>
+
+                  <Typography className={classes.day}>
+                     Monday
+                  </Typography>
+
+                  <Typography className={classes.date}>
+                     28 Sep
+                  </Typography>
+
+                  <Box component="div" className={classes.bookingDetail}>
+                     <Box component="div" className={classes.bookingList}>
+                        <Typography className={classes.time}>
+                           13.00 - 14.00
+                        </Typography>
+                        <Typography className={classes.bookingTitle}>
+                           Lunch with Petr
+                        </Typography>
+                     </Box>
+                     <Box component="div" className={classes.bookingList}>
+                        <Typography className={classes.time}>
+                           15.00 - 16.00
+                        </Typography>
+                        <Typography className={classes.bookingTitle}>
+                           Sale Weekly Meeting
+                        </Typography>
+                     </Box>
+                  </Box>
                </div>
             </div>
 
             <div className={classes.right}>
-               <AppBar position="static" className={classes.rightHeader} elevation={1}>
-                  <Toolbar className={classes.week}>
-                     <Button >this week</Button>
-                     <Button >next week</Button>
-                     <Button >whole month</Button>
-                  </Toolbar>
-               </AppBar>
-               <div >
-                  <Typography variant="body2" >
-                     today(Mon,28 Sep)
-               </Typography>
-               </div>
+               <BrowserRouter>
+                  <Box component="div" className={classes.rightHeader}>
+                     <nav className={classes.menu}>
+                        <Link className={classes.menuLink}>this week</Link>
+                        <Link className={`${classes.menuLink} ${classes.menuLinkGap}`}>next week</Link>
+                        <Link className={`${classes.menuLink} ${classes.menuLinkGap}`}>whole month</Link>
+                     </nav>
+                  </Box>
+               </BrowserRouter>
+
+               <Box component="div">
+                  <Box component="div" className={classes.dayLabel}>
+                     <Typography className={classes.dateLabel} >
+                        Today(Mon,28 Sep)
+                  </Typography>
+                  </Box>
+
+                  <Box component="div" className={classes.bookingDetailRight}>
+                     <Box component="div" className={classes.bookingListRight}>
+                        <Box component="div">
+                           <Typography className={classes.marking} style={{ color: "#3DC7D2" }}>
+                              ⬤
+                     </Typography>
+                        </Box>
+                        <Box component="div" style={{ marginLeft: "30px" }}>
+                           <Typography className={classes.timeRight}>
+                              13.00 - 14.00
+                        </Typography>
+                           <Typography className={classes.bookingTitleRight}>
+                              Lunch with Petr
+                        </Typography>
+                        </Box>
+                     </Box>
+                  </Box>
+
+                  <Box component="div" className={classes.bookingDetailRight}>
+                     <Box component="div" className={classes.bookingListRight}>
+                        <Box component="div">
+                           <Typography className={classes.marking} style={{ color: "#23CF5F" }}>
+                              ⬤
+                     </Typography>
+                        </Box>
+                        <Box component="div" style={{ marginLeft: "30px" }}>
+                           <Typography className={classes.timeRight}>
+                              13.00 - 14.00
+                        </Typography>
+                           <Typography className={classes.bookingTitleRight}>
+                              Lunch with Petr
+                        </Typography>
+                        </Box>
+                     </Box>
+                  </Box>
+               </Box>
+
+               <Box component="div">
+                  <Box component="div" className={classes.dayLabel}>
+                     <Typography className={classes.dateLabel} >
+                        Tomorrow(Tue,29 Sep)
+                  </Typography>
+                  </Box>
+
+                  <Box component="div" className={classes.bookingDetailRight}>
+                     <Box component="div" className={classes.bookingListRight}>
+                        <Box component="div">
+                           <Typography className={classes.marking} style={{ color: "#3DC7D2" }}>
+                              ⬤
+                     </Typography>
+                        </Box>
+                        <Box component="div" style={{ marginLeft: "30px" }}>
+                           <Typography className={classes.timeRight}>
+                              13.00 - 14.00
+                        </Typography>
+                           <Typography className={classes.bookingTitleRight}>
+                              Lunch with Petr
+                        </Typography>
+                        </Box>
+                     </Box>
+                  </Box>
+
+                  <Box component="div" className={classes.bookingDetailRight}>
+                     <Box component="div" className={classes.bookingListRight}>
+                        <Box component="div">
+                           <Typography className={classes.marking} style={{ color: "#23CF5F" }}>
+                              ⬤
+                     </Typography>
+                        </Box>
+                        <Box component="div" style={{ marginLeft: "30px" }}>
+                           <Typography className={classes.timeRight}>
+                              13.00 - 14.00
+                        </Typography>
+                           <Typography className={classes.bookingTitleRight}>
+                              Lunch with Petr
+                        </Typography>
+                        </Box>
+                     </Box>
+                  </Box>
+               </Box>
             </div>
-
-
          </div>
       </div>
    )
